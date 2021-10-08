e806eac2dc6d012492fb6a853da7be2bae8915a3
m.sobiczewski <m.sobiczewski@merixstudio.com>
2021-10-08 16:11:11 +0200

    settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -26,9 +26,9 @@ environ.Env.read_env(env_file=root(".env"))  # reading .env file
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
